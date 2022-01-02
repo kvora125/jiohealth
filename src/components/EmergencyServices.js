305d634b79606885ad8bf05a5ef3f7d8483b2187
kvora125 <41047220+kvora125@users.noreply.github.com>
2022-01-02 18:25:11 +0530

    Lab Test and Bed Booking Added

--- src/components/EmergencyServices.js
+++ src/components/EmergencyServices.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Row,Col, Button, Avatar,Modal, Form, Input, Checkbox, Radio }  from "antd";
+import React, { useState } from "react";
+import { Row,Col, Button, Avatar,Modal, Form, Input, Checkbox, Radio, Select, Card, Descriptions, Table }  from "antd";
 import ambulance from '../Assets/icons/EmergencyServices/ambulance.png'
 import consultation from '../Assets/icons/EmergencyServices/consultation.png'
 import hospitalBed from '../Assets/icons/EmergencyServices/hospitalBed.png'
@@ -7,12 +7,162 @@ import insurance from '../Assets/icons/EmergencyServices/insurance.png'
 import labTests from '../Assets/icons/EmergencyServices/labTests.png'
 import medicines from '../Assets/icons/EmergencyServices/medicines.png'
 
+const {Option}=Select;
+
 const EmergencyServices =() =>{
+    const [searchCity,setSearchCity]=useState("Delhi");
+    const [hospitalModalVisible,setHospitalModalVisible]=useState(false);
+    const [labTestModalVisible,setLabTestModalVisible]=useState(false);
+    const hospitalData ={
+        Ahmedabad:{
+            "Vijay Shalby Hospital": {
+                name:"Vijay Shalby Hospital",
+                address:"Fire Station, 6, 120 Feet Ring Rd, Near Memnagar, Rupam Society, Navrangpura, Ahmedabad, Gujarat 380009",
+                facilities:["multi specialty health care",],
+                costs:{
+                    "Normal Bed":"2500",
+                    "ICU Bed": "4500",
+                    "Maternity Bed":"3000",
+                },
+                availability: "random",
+                rating: 4.5,
+            },
+            "Kalrav Children Hospital": {
+                name:"Kalrav Children Hospital",
+                address:"Memnagar, Ahmedabad, Gujarat 380052",
+                facilities:["Children vaccine and healthcare"],
+                costs:{
+                    "Normal Bed":"1900",
+                },
+                availability: "random",
+                rating: 5,
+            },
+            "Sterling Hospital": {
+                name:"Sterling Hospital",
+                address:"Sterling Hospital Rd, Near maharaja agrasen vidhyalaya, L.K Society, Nilmani Society, Memnagar, Ahmedabad, Gujarat 380052",
+                facilities:["Covid Hospital"],
+                costs:{
+                    "Normal Bed":"3500",
+                    "ICU Bed": "6000",
+                    "Maternity Bed":"5000",
+                },
+                availability: "random",
+                rating: 5,
+            },
+        },
+        Delhi:{
+            "Park Hospital": {
+                name:"Park Hospital",
+                address:"Bus Depot, Meera Enclave Outer Ring Road Near Kashopur, New Chaukhandi, Vishnu Garden, New Delhi, Delhi 110018",
+                facilities:["On call Nurse"," X Ray, ICU", "Speciality Wards"],
+                costs:{
+                    "Normal Bed":"5000",
+                    "ICU Bed": "10000",
+                    "Maternity Bed":"7500",
+                },
+                availability: "random",
+                rating: 4.6,
+            },
+            "Aashlok": {
+                name:"Aashlok",
+                address:"Aashlok 25 AB, Safdarjung Enclave, New Delhi, Delhi 110029",
+                facilities:["ICU", "Normal Ward"],
+                costs:{
+                    "Normal Bed":"2300",
+                    "ICU Bed": "4500",
+                },
+                availability: "random",
+                rating: 3.8,
+            },
+            "Max Multi Speciality Centre": {
+                name:"Max Multi Speciality Centre",
+                address:"N 110, Block N, Panchsheel Park North, Panchsheel Park, New Delhi, Delhi 110017",
+                facilities:["Eye Care", "Gynaecology", "IVF", "Dental", "ENT", "Obstetrics", "Health and Wellness"],
+                costs:{
+                    "Normal Bed":"3400",
+                    "Maternity Bed":"6000",
+                },
+                availability: "random",
+                rating: 4.2,
+            },
+        },
+        Mumbai:{
+            "Lilavati Hospital And Research Centre": {
+                name:"Lilavati Hospital And Research Centre",
+                address:"A-791, Bandra Reclamation Rd, General Arunkumar Vaidya Nagar, Bandra West, Mumbai, Maharashtra 400050",
+                facilities:["COVID-19 hospital"],
+                costs:{
+                    "Normal Bed":"10000",
+                    "ICU Bed": "18000",
+                    "Maternity Bed":"12000",
+                },
+                availability: "random",
+                rating: 4.5,
+            },
+            "Zen Multi Speciality Hospital": {
+                name:"Zen Multi Speciality Hospital",
+                address:"Plot No, 425, 10th Rd, near Sandu Garden, Jai Ambe Nagar, Chembur Gaothan, Chembur, Mumbai, Maharashtra 400071",
+                facilities:["Normal Bed", "Respiratory Care","Heartcare"],
+                costs:{
+                    "Normal Bed":"10000",
+                    "ICU Bed": "18000",
+                    "Maternity Bed":"12000",
+                },
+                availability: "random",
+                rating: 4.5,
+            },
+            "Wockhardt Hospitals, Mumbai Central": {
+                name:"Wockhardt Hospitals, Mumbai Central",
+                address:"A-791, Bandra Reclamation Rd, General Arunkumar Vaidya Nagar, Bandra West, Mumbai, Maharashtra 400050",
+                facilities:["COVID-19 hospital"],
+                costs:{
+                    "Normal Bed":"6000",
+                    "ICU Bed": "12000",
+                    "Maternity Bed":"9000",
+                },
+                availability: "random",
+                rating: 4.2,
+            },
+        },
+    }
+    const labTestData=[
+        {name:"Erethrocyte Count (RBC Count)", cost: 120,},
+        {name:"LFT", cost: 250,},
+        {name:"Platelet Count (RBC Count)", cost: 100,},
+        {name:"Vitamin B12", cost: 430,},
+        {name:"Complete Blood Count (CBC) - EDTA Whole Blood", cost: 450,},
+        {name:"Lipid Profile", cost: 350,},
+        {name:"Thyroid Profile", cost: 270,},
+        {name:"Covid IgG AntiBody", cost: 120,},
+        {name:"Covid RTPCR/Covid Swab Test", cost: 1000,},
+    ];
+    const labTestTableColumns = [
+        {
+          title: 'Test Name',
+          dataIndex: 'name',
+          key: 'name',
+        },
+        {
+          title: 'Cost',
+          dataIndex: 'cost',
+          key: 'cost',
+        },
+        {
+          title: 'Action',
+          key: 'address',
+          render:(text, record) => (
+              <Button onClick={()=>{
+                  Modal.success({title:`${record.name} Test Booked Succcessfully. Further Dtails have been Mailed to you and sent in an sms on your registered mobile number. We wil contact you for finalising time and other necesssary details.`});
+                  setLabTestModalVisible(false);
+              }}>BOOK</Button>
+          )
+        },
+      ];
     return (
         <div>
             <Row justify="center">
                     <Col>
-                        <Button block='true' style={{height:'100%', width:'Auto' , margin:'10px', outline:'none', border: 'none'}}>
+                        <Button onClick={()=>{setHospitalModalVisible(true); }} block='true' style={{height:'100%', width:'Auto' , margin:'10px', outline:'none', border: 'none'}}>
                         <Row>
                             <Col>
                                 <Avatar
@@ -36,7 +186,7 @@ const EmergencyServices =() =>{
                         </Button>
                     </Col>
                     <Col>
-                        <Button style={{height:'100%', width:'Auto', margin:'10px', outline:'none', border: 'none'}}>
+                        <Button onClick={()=>setLabTestModalVisible(true) } style={{height:'100%', width:'Auto', margin:'10px', outline:'none', border: 'none'}}>
                         <Row>
                             <Col>
                                 <Avatar
@@ -222,6 +372,66 @@ const EmergencyServices =() =>{
                         </Button>
                     </Col>
             </Row>
+            <Modal
+                visible={hospitalModalVisible}
+                width={'100vw'}
+                footer={[]}
+                onCancel={()=>{
+                        setHospitalModalVisible(false);
+                    }
+                }
+            >
+                <Form.Item label=" Select A City :" style={{marginTop:'10px', marginLeft:'50px',width:'50%'}}>
+                    <Select style={{marginLeft:'10px'}} defaultValue={searchCity} onChange={(value)=>{setSearchCity(value);}}>
+                        {Object.keys(hospitalData).map(key=>{
+                            return (<Option key={key} value={key}>{key}</Option>)
+                        })}
+                    </Select>
+                </Form.Item>
+                {Object.keys(hospitalData[searchCity]).map(key=>{
+                    return(
+                        <Card style={{marginTop:'10px'}} key={hospitalData[searchCity][key]['name']} title={hospitalData[searchCity][key]['name']}>
+                            <Descriptions bordered size="small" layout="vertical">
+                            <Descriptions.Item label="Address">{hospitalData[searchCity][key]['address']}</Descriptions.Item>
+                            <Descriptions.Item label="Facilities">{hospitalData[searchCity][key]['facilities'].join(", ")}</Descriptions.Item>
+                            <Descriptions.Item label="Availability">{hospitalData[searchCity][key]['availability']}</Descriptions.Item>
+                            <Descriptions.Item label="Rating">{hospitalData[searchCity][key]['rating']}</Descriptions.Item>
+                            <Descriptions.Item label="Bed Costs">
+                            <Radio.Group defaultValue="Normal Bed">
+                                {Object.keys(hospitalData[searchCity][key]['costs']).map(subkey=>{
+                                    return(<Radio key={key+subkey} value={subkey}>{subkey}: {hospitalData[searchCity][key]['costs'][subkey]}</Radio>)
+                                })}
+                            </Radio.Group>
+                            <Button onClick={()=>{
+                                Modal.success({title:"Hospital bed booked with booking ID: XYZ XZY YXZ. Further details have been sent to your mobile and email-ID for security purposes"});
+                                setHospitalModalVisible(false);
+                            }}>
+                                Book
+                            </Button>
+                            </Descriptions.Item>
+                            </Descriptions>
+                        </Card>
+                    )
+                })}
+            </Modal>
+            <Modal
+                visible={labTestModalVisible}
+                width={'100vw'}
+                footer={[]}
+                onCancel={()=>{
+                        setLabTestModalVisible(false);
+                    }
+                }
+            >
+                <Form.Item label=" Select A City :" style={{marginTop:'10px', marginLeft:'50px',width:'50%'}}>
+                    <Select style={{marginLeft:'10px'}} defaultValue={searchCity} onChange={(value)=>{setSearchCity(value);}}>
+                        {Object.keys(hospitalData).map(key=>{
+                            return (<Option key={key} value={key}>{key}</Option>)
+                        })}
+                    </Select>
+                </Form.Item>
+                <Table dataSource={labTestData} columns={labTestTableColumns} />
+            </Modal>
         </div>
     );
 }
--- src/pages/DashBoard.js
+++ src/pages/DashBoard.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import logo from '.././logo.svg'
 import { Tabs, Button } from 'antd';
-import { NavLink } from 'react-router-dom';
 import FitnessTracker from '../components/FitnessTracker';
 import EmergencyServices from '../components/EmergencyServices';
 import Bookings from '../components/Bookings'
@@ -14,11 +13,11 @@ const Dashboard = ({setLogIn}) =>{
         <img style={{display:'inline'}} src={logo} className="App-logo-small" alt="logo" />  <Button onClick={()=>setLogIn(false)} style={{float:'right', margin:'20px'}}>LOGOUT</Button>
         </div>
         <Tabs centered="true" animated="true" size="large"  destroyInactiveTabPane="true" type="card">
+        <TabPane style={{ minHeight: '70vh'}} tab="Emergency Services" key="2" >
+              <EmergencyServices />
+          </TabPane>
           <TabPane style={{ minHeight: '70vh'}} tab="Fitness Tracker" key="1">
               <FitnessTracker />
-          </TabPane>
-          <TabPane style={{ minHeight: '70vh'}} tab="Emergency Services" key="2" >
-              <EmergencyServices />
           </TabPane>
           <TabPane style={{ minHeight: '70vh'}} tab="My Bookings" key="3" >
               <Bookings />
